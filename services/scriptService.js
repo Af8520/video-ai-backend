aa273d259b337b8e947130b66c746dc98183a399
Af8520 <148156497+Af8520@users.noreply.github.com>
2025-05-12 16:12:37 +0300

    Update scriptService.js
    change api_version header to-'Runway-API-Version': '2024-05-01'

--- services/scriptService.js
+++ services/scriptService.js
@@ -221,7 +221,7 @@ async function generateVideo(imageUrl, videoPrompt) {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${apiKey}`,
-        'X-Runway-API-Version': '2024-05-01'
+        'Runway-API-Version': '2024-05-01'
       }
     });
 
